2e00c5e9cd7a3c38979fa532440ba830161e4066
Jessy <you@example.com>
2025-11-05 16:20:13 +0100

    Update: Template mail for colors

--- src/email/mail.module.ts
+++ src/email/mail.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mailer.service';
 import { MailgunService } from './mailgun.service';
 import { EmailTemplateService } from './email-template.service';
+import { PrismaModule } from '../database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [MailService, MailgunService, EmailTemplateService],
   exports: [MailService, EmailTemplateService],
 })
