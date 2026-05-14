7969471f4d144ae8c44ac572e0a6c650fde94013
Jessy <you@example.com>
2026-05-14 16:17:34 +0200

    Add: Model prisma & module Stats Visite salon

--- src/salon-analytics/salon-analytics.module.ts
+++ src/salon-analytics/salon-analytics.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SalonAnalyticsService } from './salon-analytics.service';
 import { SalonAnalyticsController } from './salon-analytics.controller';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
-  providers: [SalonAnalyticsService],
+  providers: [SalonAnalyticsService, PrismaService],
   controllers: [SalonAnalyticsController],
   exports: [SalonAnalyticsService],
 })
